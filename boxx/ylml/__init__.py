bbd69d532de306f21d74007e3cc7169fe0215aa3
Lei <ylxx@live.com>
2021-10-12 16:18:50 +0800

    rm ylml*

--- boxx/ylml/__init__.py
+++ boxx/ylml/__init__.py
@@ -6,14 +6,14 @@ tools for Machine Learning and Image Processing
 """
 from __future__ import unicode_literals
 
-from .ylmlTrain import GenSimg
+# from .ylmlTrain import GenSimg
 
-from .ylmlTest import (binaryDiff, classDiff, drawBoundAndBackground, 
-                      confusionMatrix, f1Score,
-                      getWeightCore, smallImg, autoSegmentWholeImg,
-                      ArgList, autoFindBestEpoch, autoFindBestParams)
+# from .ylmlTest import (binaryDiff, classDiff, drawBoundAndBackground, 
+#                       confusionMatrix, f1Score,
+#                       getWeightCore, smallImg, autoSegmentWholeImg,
+#                       ArgList, autoFindBestEpoch, autoFindBestParams)
 
-from .ylmlEvalu import (Evalu, accEvalu, lplrEvalu, diceEvalu, pd)
+# from .ylmlEvalu import (Evalu, accEvalu, lplrEvalu, diceEvalu, pd)
 
 from .ylDete import Bbox, Canvas, cropMinAreaRect, loadCoco, saveCoco
 
